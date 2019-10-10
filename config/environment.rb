def824559bc6f04a360bc08ac31f52e9de94e23c
Hong Phuc <og.viet.003.dev@gmail.com>
2019-10-10 16:41:47 +0700

    Fix error adding wrong codes

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,4 @@
 # Load the Rails application.
 require_relative 'application'
-
 # Initialize the Rails application.
 Rails.application.initialize!
-
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
